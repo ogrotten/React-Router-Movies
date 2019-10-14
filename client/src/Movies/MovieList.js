aa0f2876f37b2bbabcc8b0004a4b4a3c4322e063
Darrin Lowery <lambda@monsto.com>
2019-10-14 16:41:49 -0500

    complete

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -24,19 +24,33 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`} key={movie.id}><MovieDetails movie={movie} /></Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails({ movie }) {
-  // const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`/movies/${movie.id}`}>
-      <MovieCard props={movie}/>
-    </Link>
-  );
+  const { title, director, metascore, stars } = movie;
+  //   <Link to={`/movies/${movie.id}`}>
+	// let { director, id, metascore, stars, title } = props;
+	return (
+		<div className="movie-card">
+			<h2>{title}</h2>
+			<div className="movie-director">
+				Director: <em>{director}</em>
+			</div>
+			<div className="movie-metascore">
+				Metascore: <strong>{metascore}</strong>
+			</div>
+			<h3>Actors</h3>
+			{stars.map(star => (
+				<div key={star} className="movie-star">
+					{star}
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default MovieList;

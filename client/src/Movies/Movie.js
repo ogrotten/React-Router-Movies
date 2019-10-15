7ee7791dc520e6aa1eb31dea85a6655e10346b05
Darrin Lowery <lambda@monsto.com>
2019-10-15 16:51:26 -0500

    .

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -22,10 +22,10 @@ const Movie = props => {
 	}, []);
 
 	// Uncomment this only when you have moved on to the stretch goals
-	// const saveMovie = () => {
-	//   const addToSavedList = props.addToSavedList;
-	//   addToSavedList(movie)
-	// }
+	const saveMovie = () => {
+	  const addToSavedList = props.addToSavedList;
+	  addToSavedList(movie)
+	}
 
 	if (!movie) {
 		return <div>Loading movie information...</div>;
